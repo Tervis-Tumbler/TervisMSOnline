80d823b8a6994d8c40e90af96996e18a58c53192
ChrisMagnuson <hourglasssand@gmail.com>
2018-01-12 14:19:17 -0500

    Initial commit of spam definition

---
+++ SpamDefinition.ps1
@@ -0,0 +1,12 @@
+﻿$SpamDomainDefinition = [PSCustomObject][Ordered]@{
+    Domain = "karyatechsolutions.com"
+    Reason = "Unsolicited email"
+},
+[PSCustomObject][Ordered]@{
+    Domain = "erpmaestro.com"
+    Reason = "Unsolicited email"
+},
+[PSCustomObject][Ordered]@{
+    Domain = "softwareleadsusa.com"
+    Reason = "Unsolicited email"
+}
