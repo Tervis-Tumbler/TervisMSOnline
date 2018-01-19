8dac8bd82649c264a8658abb715f5b742a7b7fac
ChrisMagnuson <hourglasssand@gmail.com>
2018-01-19 09:45:16 -0500

    Add spam emails

--- SpamDefinition.ps1
+++ SpamDefinition.ps1
@@ -44,6 +44,51 @@ Kaseya <kaseyanews@kaseya.com>
 Matt Bender <mbender@mainstreetdbas.com>
 Ken Candela <kenc@kynetictech.com>
 Information Management <msgs@product.information-management.com>
+Jenna Kuphall <jenna.kuphall@neuronesb.com>
+SentinelOne <info@sentinelone.com>
+Alert Logic <mkto@alertlogic.com>
+Mills Horton <mhorton@scivera.com>
+ft1036@feitongexp.com
+1WorldSync News <news@1worldsync.com>
+Jeff Ellerbee, Planview <J.Ellerbee@Planview.com>
+Executive Speaking <info@execspeak.com>
+Analytics add-on <mail@businesspressdistribution.com>
+Tyler Roxby <troxby@wombatsecurity.com>
+Prem Kumar Pollapalli <prem@evoketechnologies.com>
+Cameron Vollerthum <cvollerthum@enablingtechcorp.com>
+Mary Rose <mary.rose@cyberark.com>
+Brian Elswick <brian.elswick@sovos.com>
+John Turner <jturner@securelink.com>
+robinc@epcgroup.net
+Emily Hewitt <emily.h@logz.io>
+Dundas Data Visualization <info@dundas.com>
+Shaporenko, Alexei <ashaporenko@verisign.com>
+Clemens, Mike (01070) <mike.clemens@roberthalf.com>
+Bernard Hall <bernard.hall@softwareleadsusa.com>
+Nikola Metes <nikola.metes@bonitasoft.com>
+Michelle, Contrast Security <michelle.edwards@contrastsecurity.com>
+Susie Wee <devnet-susiew=cisco.com@mail180.atl221.rsgsv.net>
+Maya Anderson <maya.anderson@dataweave.net>
+OpenText <direct@opentext.com>
+SecureWorkComp <Daniel@secureworkcomp.net>
+Travis Smith <tsmith@highmarkrecruiting.com>
+survey-noreply@mr.surveymonkeyuser.com
+Consultis <info@consultis.com>
+Michael Nocerino, 451 Alliance <Membership@451Alliance.com>
+Rachel Dines <rachel@cloudhealthtech.com>
+Nutanix <success@nutanix.com>
+Mitel Blog <blog@mitel.com>
+ConnectWise Inc. <newsletter@e.connectwise.com>
+Simon Bitton <sbitton@parkplacetech.com>
+Robert Reinhardt <rreinhardt@silver-peak.com>
+Ehlinger, Tim <TEhlinger@kforce.com>
+Susan Magi <susan.magi@enterworks.com>
+Redmond Enterprise IT Insights Newsletter <Redmond@1105data.com>
+Angela Guzman <Angela.Guzman@erpmaestro.com>
+Paula Fretter <reply@thetotalemaildelivery.com>
+Chris Genazzio <chris@mdsny.com>
+Eric <eric@mjvtechnology.com>
+Combodo <contact=combodo.com@mail159.atl221.rsgsv.net>
 "@ -split "`r`n"
     Reason = "Unsolicited email"
 }
